cc723256f20ad85db13533bbd8d420426c0c23be
tifanymeunier <tifany.meunier@etu.univ-orleans.fr>
2025-01-30 08:51:28 +0100

    Q3 finie

---
+++ Dionysies/Auteur.java
@@ -0,0 +1,82 @@
+class Auteur {
+  String nom;
+
+  String citation_tragédie;
+  int qualité_tragédie;
+
+  String citation_comédie;
+  int qualité_comédie;
+
+  String citation_drame;
+  int qualité_drame;
+
+  public Auteur(
+      String nom,
+      int qualité_tragédie,
+      String citation_tragédie,
+      int qualité_comédie,
+      String citation_comédie,
+      int qualité_drame,
+      String citation_drame) {
+    this.nom = nom;
+    this.qualité_comédie= qualité_comédie;
+    this.qualité_drame= qualité_drame;
+    this.qualité_tragédie = qualité_tragédie;
+    this.citation_comédie=citation_comédie;
+    this.citation_drame=citation_drame;
+    this.citation_tragédie=citation_tragédie;
+  }
+
+  public String getNom() {
+    return this.nom;
+  }
+
+  public String getCitation_tragédie() {
+    return this.citation_tragédie;
+  }
+
+  public int getQualité_tragédie() {
+    return this.qualité_tragédie;
+  }
+
+  public String getCitation_comédie() {
+    return this.citation_comédie;
+  }
+
+  public int getQualité_comédie() {
+    return this.qualité_comédie;
+  }
+
+  public String getCitation_drame() {
+    return this.citation_drame;
+  }
+
+  public int getQualité_drame() {
+    return this.qualité_drame;
+  }
+
+  @Override
+  public String toString(){
+    return "L'honorable "+this.nom;
+  }
+
+
+
+
+  public Style point_fort() {
+    if (qualité_tragédie > qualité_comédie) {
+      if (qualité_drame > qualité_tragédie) {
+        return Style.DRAME;
+      } else {
+        return Style.TRAGÉDIE;
+      }
+    } else {
+      if (qualité_drame > qualité_comédie) {
+        return Style.DRAME;
+      } else {
+        return Style.COMÉDIE;
+      }
+    }
+  }
+
+}
